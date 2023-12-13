0a96667634cd8d5605b11e57b487a1b472ae69df
Ankur Seth <iankurseth@gmail.com>
2023-12-13 16:09:21 +0530

    Implemented a basic helpCommand()

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,6 +1,17 @@
 
 
 
+const commands = [
+	{command: "add", shortcut: "a", description: "Add an icon."},
+	{command: "remove", shortcut: "r", description: "Remove an icon."},
+	{command: "list", shortcut: "l", description: "List all icons."},
+	{command: "search", shortcut: "s", description: "Search for an icon."},
+	{command: "help", shortcut: "h", description: "Just show some help."},
+	{command: "version", shortcut: "v", description: "Show version info."}
+];
+
 export function helpCommand (rest: string[]) {
-	console.log(`This is redicons-cli "help" command!`);
+	for (const command of commands) {
+		console.log(`| ${command.command.padEnd(15)} | ${command.shortcut.padEnd(4)} | ${command.description.padEnd(40)} |`);
+	}
 }
