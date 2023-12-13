89a47daaf1ea440ae8cf5a8dea31ac98cafedcb5
Ankur Seth <iankurseth@gmail.com>
2023-12-13 16:02:00 +0530

    Implemented searchCommand()

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,6 +1,21 @@
+import { icons } from "./common";
 
 
 
 export function searchCommand (rest: string[]) {
-	console.log(`This is redicons-cli "search" command!`);
+	const resultIcons = icons.filter(icon => {
+		for (const arg of rest) {
+			if (icon.name.includes(arg)) {
+				return true;
+			}
+		}
+		return false;
+	});
+
+	console.log(`Found ${resultIcons.length} icons matching your query.`);
+	let idx = 0;
+	for (const icon of resultIcons) {
+		console.log(`\t${idx+1}. ${icon.name}`);
+		idx++;
+	}
 }
