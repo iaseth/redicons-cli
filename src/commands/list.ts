4c902f244fcd5bacb83b30a339d450e200c22fc8
Ankur Seth <iankurseth@gmail.com>
2023-12-13 16:54:57 +0530

    Implemented listCommand()

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,6 +1,14 @@
+import { getIconsFromFile, outputJsonPath } from "./common";
 
 
 
 export function listCommand (rest: string[]) {
-	console.log(`This is redicons-cli "list" command!`);
+	const existingIcons = getIconsFromFile();
+	console.log(`Found ${existingIcons.length} icons in ${outputJsonPath}!`);
+
+	let idx = 0;
+	for (const icon of existingIcons) {
+		console.log(`\tIcon ${idx+1}/${existingIcons.length}: "${icon.name}"`);
+		idx++;
+	}
 }
