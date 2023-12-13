1fb784bb45bfde22ac110d93b9f99a8bcfc017ce
Ankur Seth <iankurseth@gmail.com>
2023-12-13 16:11:46 +0530

    Implemented a basic versionCommand()

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,6 +1,7 @@
+import { icons } from "./common";
 
 
 
 export function versionCommand (rest: string[]) {
-	console.log(`This is redicons-cli "version" command!`);
+	console.log(`This is redicons-cli with ${icons.length} icons!`);
 }
