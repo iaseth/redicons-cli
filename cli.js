a0742524390b0f07793ca1acca80646599ca4cbd
Ankur Seth <iankurseth@gmail.com>
2023-12-13 14:47:14 +0530

    Added icons JSON to repo

--- cli.js
+++ cli.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+const fs = require("fs");
+
+const rediconsJson = require("./redicons.json");
+const icons = rediconsJson.icons;
 
 
 
 function main () {
-	console.log("Thanks for running JOSN.");
+	const [command, ...rest] = process.argv.slice(2);
+	console.log(`There are ${icons.length} icons ready for use.`);
 }
 
 main();
