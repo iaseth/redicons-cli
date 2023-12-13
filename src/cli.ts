51b62baf73fb0c4e5ceae8314bfb47852deb69b3
Ankur Seth <iankurseth@gmail.com>
2023-12-13 15:02:29 +0530

    Now using TS

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-const fs = require("fs");
-
-const rediconsJson = require("./redicons.json");
+import rediconsJson from "./redicons.json";
 const icons = rediconsJson.icons;
 
 
@@ -9,6 +7,7 @@ const icons = rediconsJson.icons;
 function main () {
 	const [command, ...rest] = process.argv.slice(2);
 	console.log(`There are ${icons.length} icons ready for use.`);
+	console.log(`Now redicons-cli is using TypeScript.`);
 }
 
 main();
